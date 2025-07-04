f252b9174ec0c9e044f26d8ff346ca7333f4b114
Dean Blackborough <dean@g3d-development.com>
2025-07-04 23:14:21 +0100

    Playing with animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var player_speed: float = 175.0;
-@export var player_jump_force: float = -350;
+@export var player_jump_force: float = -375;
 @export var gravity: float = 25;
 @export var terminal_velocity: float = 500; 
 
@@ -18,17 +18,34 @@ func _physics_process(delta):
 	
 	var player_direction = Input.get_axis("moveLeft", "moveRight");
 	
-	draw_sprite(player_direction, player_speed);
-	
 	velocity.x = player_speed * player_direction;
+	
+	draw_player_sprite(
+		player_direction, 
+		player_speed,
+		velocity.y,
+		is_on_floor()
+	);
+	
 	move_and_slide();
 	
 	print(player_direction);
 	print(player_speed);
 	print(velocity);
 	
-func draw_sprite(player_direction: float, player_speed: float):
-	if player_direction != 0:
-		$AnimatedSprite2D.scale.x = player_direction;
-		
-	
+func draw_player_sprite(
+	player_direction: float, 
+	player_speed: float,
+	player_velocity_y: float, 
+	player_on_floor: bool
+):
+	if not player_on_floor:
+		if player_velocity_y < 0:
+			$AnimatedSprite2D.animation = "jump"
+			## Add fall animation
+	else:
+		if player_direction != 0:
+			$AnimatedSprite2D.scale.x = player_direction
+			$AnimatedSprite2D.animation = "run"
+		else:
+			$AnimatedSprite2D.animation = "idle"
