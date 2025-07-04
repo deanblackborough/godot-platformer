a7d1457f0a11b4d3a7c7279802cac53277c3b841
Dean Blackborough <dean@g3d-development.com>
2025-07-04 21:35:48 +0100

    Messing around

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,34 @@
 extends CharacterBody2D
 
-@export var playerSpeed = 150;
-@export var playerJumpForce = -325;
-@export var gravity = 30;
-@export var terminalVelocity = 500; 
+@export var player_speed: float = 175.0;
+@export var player_jump_force: float = -350;
+@export var gravity: float = 25;
+@export var terminal_velocity: float = 500; 
 
 func _physics_process(delta):
 	
 	if is_on_floor() == false:
 		velocity.y += gravity;
 		
-		if velocity.y > terminalVelocity:
-			velocity.y = terminalVelocity;
+		if velocity.y > terminal_velocity:
+			velocity.y = terminal_velocity;
 	
 	if Input.is_action_just_pressed("jump"):
-		velocity.y = playerJumpForce;
+		velocity.y = player_jump_force;
 	
-	var playerDirection = Input.get_axis("moveLeft", "moveRight");
-	velocity.x = playerSpeed * playerDirection;
+	var player_direction = Input.get_axis("moveLeft", "moveRight");
+	
+	draw_sprite(player_direction, player_speed);
+	
+	velocity.x = player_speed * player_direction;
 	move_and_slide();
 	
+	print(player_direction);
+	print(player_speed);
 	print(velocity);
+	
+func draw_sprite(player_direction: float, player_speed: float):
+	if player_direction != 0:
+		$AnimatedSprite2D.scale.x = player_direction;
+		
+	
